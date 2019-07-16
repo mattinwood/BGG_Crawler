e537c9882675f3544c034a827cb268cab0ec667f
Matt Inwood <matt.inwood@gmail.com>
2019-07-16 11:04:52 -0500

    Added documentation.

--- game_crawler.py
+++ game_crawler.py
@@ -34,7 +34,7 @@ def engine_builder():
     return create_engine(db_connect_url)
 
 
-def slack_message(body):
+def slack_message(body: str):
     sc = WebClient(getenv('SLACK_TOKEN'))
     sc.chat_postMessage(
         channel='scheduled-jobs',
@@ -45,13 +45,19 @@ def slack_message(body):
 #%%
 class StoneAge:
     def __init__(self, browser):
+        '''
+        Initialized class variables, including existing game list from stored file.
+
+        :param browser: Selenium webdriver object
+        '''
         self.browser = browser
         self.browser.get('https://en.boardgamearena.com')
         self.wait = WebDriverWait(self.browser, 30)
 
+        # Validates the existence of the YAML file location.
         if not path.exists('data/'):
             mkdir('data')
-
+        # If an existing list of games is present, read it in. Else, create an empty file.
         try:
             self.read_unparsed_game_ids()
             if not self.game_ids:
@@ -62,6 +68,9 @@ class StoneAge:
             open('data/new_games.yaml', 'a').close()
 
     def login(self):
+        '''
+        Checks if user is logged in, and if not, go to login page and enter credentials.
+        '''
         if 'boardgamearena.com' not in self.browser.current_url:
             self.browser.get('https://en.boardgamearena.com')
 
@@ -92,6 +101,10 @@ class StoneAge:
             print("Already logged in")
 
     def get_recent_game_ids(self):
+        '''
+        Goes to recent results page and extracts the game IDs from the top list.
+        Checks for uniqueness against loaded YAML file, and subtracts game IDs existing in the DB.
+        '''
         url = 'https://en.boardgamearena.com/#!gamepanel?game=stoneage&section=lastresults'
         self.browser.get(url)
 
@@ -110,6 +123,9 @@ class StoneAge:
         self.write_new_game_ids()
 
     def write_new_game_ids(self):
+        '''
+        Writes game IDs to YAML file for future extraction.
+        '''
         with open('data/new_games.yaml', 'w') as outfile:
             yaml.dump(self.game_ids, outfile)
 
@@ -117,15 +133,21 @@ class StoneAge:
         with open("data/new_games.yaml", 'r') as stream:
             self.game_ids = yaml.load(stream)
 
-    def game_results(self, url):
+    def game_results(self, url: str):
+        '''
+        Converts the table in the results screen to a dictionary.
+        :param url: Formatted url for the game summary
+        :return: Dictionary of the results table
+        '''
         self.browser.get(url)
         self.wait.until(EC.presence_of_element_located((By.ID, 'player_stats_table')))
-
-        table = self.browser.find_element_by_id('player_stats_table')
         results = {}
 
+        # Creates object for the tables and individual rows.
+        table = self.browser.find_element_by_id('player_stats_table')
         rows = table.find_elements_by_tag_name('tr')
 
+        # For each row in the table, create a list of values and assign it to the
         for row in rows:
             row_name = row.find_element_by_tag_name('th').text
             if row_name == '':
@@ -137,6 +159,7 @@ class StoneAge:
             if row_name != 'All stats':
                 results[row_name] = values
 
+        # Pulls additional game info from other page sources, relative to the player info.
         panel = self.browser.find_element_by_id('game_result_panel')
 
         newranks = [x.text for x in panel.find_elements_by_class_name('gamerank_value')]
@@ -147,7 +170,11 @@ class StoneAge:
 
         return results
 
-    def game_logs(self, url):
+    def game_logs(self, url: str):
+        '''
+        :param url: Formatted url for the game replay
+        :return: Raw list of actions taken during the game
+        '''
         self.browser.get(url)
         self.wait.until(EC.presence_of_element_located((By.ID, 'gamelogs')))
 
@@ -157,26 +184,37 @@ class StoneAge:
 
         return actions
 
-    def game_info(self, game_id):
+    def game_info(self, game_id: int):
+        '''
+        Main function for transforming the game summary and logs. Also outputs to Database.
+        :param game_id: Integer for the game ID, used for identification and for urls.
+        '''
+
+        # Sends slack notification that job has begun; creates url Strings
         slack_message(f'Loading game ID {game_id}')
         results_url = f'https://en.boardgamearena.com/#!table?table={game_id}'
         replay_url = f'https://en.boardgamearena.com/#!gamereview?table={game_id}'
 
+        # Return and write locally the results data.
         summary_results = self.game_results(results_url)
         pickle.dump(summary_results, open('data/results.pkl', 'wb'))
 
+        # Return and write locally the log data.
         logs = self.game_logs(replay_url)
         pickle.dump(logs, open('data/logs.pkl', 'wb'))
 
-        # Log Cleanup
+        # Assign player order by finding logs where the first player of each round switches
         player_order = list(set([x[0:x.find(' is')] for x in logs if 'is now first player' in x][0:4]))
 
+        # For each log item, assign the player ID.
         player_nums = []
         for x in logs:
+            # Abandoned games are discarded.
             if 'chose to abandon' in x.lower():
                 self.game_ids.remove(game_id)
                 return
 
+            # These log items do not have any game impact and/or are not specific to a player
             elif 'end of the game' in x.lower():
                 player_nums.append(-1)
 
@@ -194,11 +232,14 @@ class StoneAge:
                     if x.find(player_order[i]) >= 0:
                         player_nums.append(i)
 
+        # Assigns player index/number in place of name in the logs.
         for player in player_order:
             logs = [x.replace(player, 'player') for x in logs]
 
+        # For each log, replaces numbers with the character i,
+        # and creates a separate field in the table structure to capture the value;
+        # This normalizes player actions in the log table.
         values = [int((re.findall('\d+', x) or [-1])[0]) for x in logs]
-
         logs = [re.sub(r'\d', 'i', x) for x in logs]
 
         log_df = pd.DataFrame({
@@ -207,38 +248,48 @@ class StoneAge:
             'action_name': logs,
         })
 
+        # Sets flag for a new turn/round; and assigns a turn/round number using that flag.
         log_df.loc[log_df['action_name'] == 'player is now first player', 'new_turn'] = 1
         log_df['turn_number'] = log_df['new_turn'].fillna(0).cumsum()
+
+        # Sets move number as an index starting at 1.
         log_df['move_number'] = log_df.index + 1
+
         log_df['game_id'] = game_id
         log_df = log_df.drop('new_turn', axis=1)
 
+        # Creates empty lists of the appropriate length for missing data.
         for key in summary_results.keys():
             if len(summary_results[key]) == 0:
                 summary_results[key] = [None] * len(summary_results['Player Names'])
 
+        # Converts summary results into a DataFrame and does some string cleanup.
         summary_df = pd.DataFrame(summary_results)
         summary_df.columns = summary_df.columns.str.replace("'", "").str.lower().str.replace(' ', '_')
         summary_df['game_id'] = game_id
 
+        # Writes tables to the database.
         log_df.to_sql('game_logs', engine_builder(), schema='bgg', if_exists='append', index=False)
         summary_df.to_sql('game_summary', engine_builder(), schema='bgg', if_exists='append', index=False)
 
+        # Removes completed game ID from the list and writes local list of recent game IDs.
         self.game_ids.remove(game_id)
         self.write_new_game_ids()
 
-        slack_message(f'loaded game ID {game_id}')
+        slack_message(f'Loaded game ID {game_id}')
 
 def main():
+    # Initializes the display for headless use.
     display = Display(visible=0, size=(1366, 768))
     display.start()
 
+    # Instantiates the browser and logs in.
     b = StoneAge(webdriver.Firefox())
     b.login()
 
     b.get_recent_game_ids()
 
-    # playing around with rate limits, so using random for now.
+    # Select one of the recent games at random and process it.
     b.game_info(choice(list(b.game_ids)))
 
     # working_list = list(b.game_ids)[:]
